b6ebad6a2c4b80feb97142a132bb658795b2f980
veronnah <yoo.veroon@gmail.com>
2021-08-05 16:47:26 +0200

    slider added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.gallery').owlCarousel({
+    margin:10,
+    loop:false,
+    autoWidth:true,
+    items:2, 
+    nav: true, 
+    dots:false,
+    navText: ['<img src="./img/arrow-left.svg">', '<img src="./img/arrow-right.svg">'],
+});
