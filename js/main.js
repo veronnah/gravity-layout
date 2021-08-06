975d92dc80f2b98e1b4f8d361900d8724f187e58
veronnah <yoo.veroon@gmail.com>
2021-08-06 19:48:30 +0200

    desctop version done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-$('.gallery').owlCarousel({
-    margin:10,
-    loop:false,
-    autoWidth:true,
-    items:2, 
-    nav: true, 
-    dots:false,
-    navText: ['<img src="./img/arrow-left.svg">', '<img src="./img/arrow-right.svg">'],
+$(function () {
+    $('.gallery').owlCarousel({
+        margin: 10,
+        loop: false,
+        autoWidth: true,
+        items: 2,
+        nav: true,
+        dots: false,
+        navText: ['<img src="./img/arrow-left.svg">', '<img src="./img/arrow-right.svg">'],
+    });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $('.testimonials-carousel').owlCarousel({
+        loop: false,
+        items: 1,
+        nav: true,
+        dots: false,
+        navText: ['<img src="./img/arrow-left.svg">', '<img src="./img/arrow-right.svg">'],
+    });
+
 });
